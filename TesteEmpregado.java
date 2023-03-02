7ff85c00fc08a3db597bedc20c52d7bc9040d654
Dio Eguci <diogenes.eguchi@gmail.com>
2023-03-02 17:42:52 -0300

    Processamento de empregados usando uma lista

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,19 +1,29 @@
-import javax.print.attribute.SetOfIntegerSyntax;
+import java.util.ArrayList;
 
 public class TesteEmpregado {
-    public static void main (String[] args){
-        Empregado e1 = new Empregado();
-        Empregado e2 = new Empregado();
-        Empregado e3 = new Empregado();
-        e1.setTipo(tipo: 0);
-        e2.setTipo(tipo: 1);
-        e3.setTipo(tipo: 2);
-        
+  public static void main(String[] args) {
+    Empregado e1 = new Empregado();
+    Empregado e2 = new Empregado();
+    Empregado e3 = new Empregado();
+    e1.setTipo(0);
+    e2.setTipo(1);
+    e3.setTipo(2);
 
-        e1.setSalario(2000);
-        e2.setSalario(1000);
-        e1.setComissao(comissao: 0.1);
-        e3.setSalario(3000);
-        e3.setBonus(0.5);
-        }
+    e1.setSalario(2000);
+    e2.setSalario(1800);
+    e2.setComissao(0.1);
+    e3.setSalario(1700);
+    e3.setBonus(400);
+
+    var empregados = new ArrayList<Empregado>();
+    empregados.add(e1);
+    empregados.add(e2);
+    empregados.add(e3);
+    // for (Empregado e : empregados){
+    // System.out.println(e.calcularSalario());
+    // }
+    for (int i = 0; i < empregados.size(); i++) {
+      System.out.println(empregados.get(i).calcularSalario());
+    }
+  }
 }
